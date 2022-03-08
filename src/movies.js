67ecdc8763995a03e6912b4ba3a382cc6b183f1a
João Miguel Inácio <joao.inacio@gmail.com>
2022-03-08 01:04:59 +0000

    Lab solved - Bonus not completed

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,6 @@
-// Iteration 1: All directors? - Get the array of all directors. (NOT COMPLETE)
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
-// How could you "clean" a bit this array and make it unified (without duplicates)?
-
 const movies = require("./data");
 
+// Iteration 1: All directors? - Get the array of all directors. (READY - Missing BONUS)
 
 function getAllDirectors(movies) {
   let arrayOfDirectors = movies.map(x => x.director);
@@ -17,47 +14,87 @@ function howManyMovies(array) {
   if (array.length === 0){
     return 0;
   } else {
-    const stevensDramas= array.filter((x) => x.genre === "Drama" || x.genre.includes("Drama") && x.director === "Steven Spielberg");
+    const stevensDramas= array.filter((x) => x.genre.includes("Drama") && x.director === "Steven Spielberg");
     return stevensDramas.length                          
   }
 }
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals (NOT COMPLETE)
+// Iteration 3: All scores average - Get the average of all scores with 2 decimals (READY)
 
 //Function "scoresAverage" //should return average even if one of the movies does not have score
 
 function scoresAverage(array) {
-  if (array.length === 0){
+  if(array.length === 0){
     return 0;
-  } else {
-  const avg =	array.reduce((acc, val) => {
-		return acc + val.score;
-	}, 0) / array.length;
-  return (Number(avg.toFixed(2)));
   }
+  const avg = array.reduce((acc, val) => {
+    if(typeof(val.score) === "number") {
+      return acc + val.score
+    }
+    return acc
+  }, 0) / array.length
+  return Math.round(avg * 100) / 100
 }
 
-// Iteration 4: Drama movies - Get the average of Drama Movies (NOT COMPLETE)
+// Iteration 4: Drama movies - Get the average of Drama Movies (READY)
+
 function dramaMoviesScore(array) {
-  const avg =	(array.filter((x) => x.genre === "Drama" || x.genre.includes("Drama"))).reduce((acc, val) => {
-		return acc + val.score;
-	}, 0) / (array.filter((x) => x.genre === "Drama" || x.genre.includes("Drama"))).length;
-  return (Number(avg.toFixed(2)));
+    const allDramas = array.filter(function(x){
+      if(x.genre.includes("Drama")){
+        return true;
+      }
+      return false; 
+    })
+    if(allDramas.length === 0){
+      return 0;
+    } 
+    const sum = allDramas.reduce((acc, val) => {
+      if(typeof(val.score) === "number") {
+        return acc + val.score
+      }      
+      return acc 
+    },0)
+    const averageScoreDramas = sum / allDramas.length
+    return Math.round(averageScoreDramas * 100) / 100
 }
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order) (NOT COMPLETE)
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order) (READY)
 function orderByYear(array) {
-  const sortedByYearArray = array.sort((a, b) => {
-    return a.year - b.year;
-  }); return sortedByYearArray
+  const clonedArray =  JSON.parse(JSON.stringify(array))
+  const sortedByYearArray = clonedArray.sort((a, b) =>{
+    if (a.year < b.year ){
+      return -1;
+    } else if (a.year > b.year) {
+      return 1;
+    } else { 
+      if (a.title < b.title ){
+        return -1;
+      } else if (a.title > b.title) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } 
+  }) 
+  return sortedByYearArray
 }
 
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles (NOT COMPLETE)
+// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles (READY)
 function orderAlphabetically(array) {
-  const sortedAphabetically = array.sort((a, b) => {
-    return a.title - b.title;
-  }); return sortedAphabetically
+  const clonedArray =  JSON.parse(JSON.stringify(array));
+  const sortedAphabetically = clonedArray.sort((a, b) => {
+    if (a.title < b.title ){
+      return -1;
+    } else if (a.title > b.title) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+    const titles = sortedAphabetically.map(function(movie){
+      return movie.title;
+}); 
+  return titles.slice(0, 20)
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
